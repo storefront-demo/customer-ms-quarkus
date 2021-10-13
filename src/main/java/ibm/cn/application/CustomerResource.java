c64d6aa42ec16f972471e8e21909fd2b2c330d82
gkovan <gkovan@hotmail.com>
2021-10-13 02:28:41 -0400

    /micro/customer endpoint now gets customer data from the id token

--- src/main/java/ibm/cn/application/CustomerResource.java
+++ src/main/java/ibm/cn/application/CustomerResource.java
@@ -3,6 +3,7 @@ package ibm.cn.application;
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -15,6 +16,7 @@ import org.jose4j.json.internal.json_simple.JSONArray;
 import org.jose4j.json.internal.json_simple.JSONObject;
 
 import ibm.cn.application.couchdb.client.CouchDBClientService;
+import ibm.cn.application.model.Customer;
 
 @Path("/micro/customer")
 public class CustomerResource {
@@ -27,32 +29,57 @@ public class CustomerResource {
 	
 	@Inject
     JsonWebToken jwt;
-    
+
+    // get the customer data from the id_token which is in jwt token format
     @SuppressWarnings("unchecked")
-	@GET
+	@GET()
     @Produces(MediaType.APPLICATION_JSON)
 //    @RolesAllowed({"user","admin"})
-    public Response getCustomerByUsername() throws Exception{
-    	LOG.info("Get customer by username");
+    public Customer getCustomerByJWT() throws Exception{
+    	LOG.info("Get customer by jwt");
         try {
-            JSONObject body = new JSONObject();
-            JSONObject selector = new JSONObject();
-            
-            selector.put("username", jwt.getName());
-            
-            body.put("selector", selector);
-            JSONArray fields = new JSONArray();
-            body.put("fields", fields);
-            body.put("limit", 1);
-            Response r = cdb.getUsername(body);
-            r.getHeaders().remove("Transfer-Encoding");
-            return r;
-
+            Customer cust = new Customer();
+            LOG.info("printing the jwt token");
+            LOG.info(jwt.toString());
+            jwt.getName();
+            cust.setEmail(jwt.getClaim("email"));
+            cust.setLastName(jwt.getClaim("family_name"));
+            cust.setFirstName(jwt.getClaim("given_name"));
+            cust.setUsername(jwt.getClaim("preferred_username"));
+            return cust;
         }
-        catch (Exception e){
+        catch (Exception e) {
             e.printStackTrace();
-            System.out.println(Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getLocalizedMessage()).build());
             throw new Exception(e.toString());
         }
     }
+
+    // Get the customer data from the CouchDB database
+//     @SuppressWarnings("unchecked")
+// 	@GET
+//     @Produces(MediaType.APPLICATION_JSON)
+// //    @RolesAllowed({"user","admin"})
+//     public Response getCustomerByUsername() throws Exception{
+//     	LOG.info("Get customer by username");
+//         try {
+//             JSONObject body = new JSONObject();
+//             JSONObject selector = new JSONObject();
+            
+//             selector.put("username", jwt.getName());
+            
+//             body.put("selector", selector);
+//             JSONArray fields = new JSONArray();
+//             body.put("fields", fields);
+//             body.put("limit", 1);
+//             Response r = cdb.getUsername(body);
+//             r.getHeaders().remove("Transfer-Encoding");
+//             return r;
+
+//         }
+//         catch (Exception e){
+//             e.printStackTrace();
+//             System.out.println(Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getLocalizedMessage()).build());
+//             throw new Exception(e.toString());
+//         }
+//     }
 }
